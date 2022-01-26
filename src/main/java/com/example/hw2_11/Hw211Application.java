87f9f82a754184de421dbbcbcb5bf2338f272b1d
estonec <rusecru@gmail.com>
2022-01-26 15:47:00 +0300

    Initial commit

---
+++ src/main/java/com/example/hw2_11/Hw211Application.java
@@ -0,0 +1,13 @@
+package com.example.hw2_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw211Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hw2_11/Hw211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw2_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
